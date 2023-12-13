7dda2c025556654864fb0cee2a8cff0dff53e805
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2023-12-13 16:33:35 -0600

    mod to ammender

--- src/ammender/ammender.rs
+++ src/ammender/ammender.rs
@@ -8,7 +8,7 @@ struct CommitData {
     date: String
 }
 
-pub fn amend_commits(json_file: &str, new_author: &str, new_email: &str) -> Result<(), String> {
+pub fn amend_commits(repo_path: &str, json_file: &str, new_author: &str, new_email: &str) -> Result<(), String> {
     let data = fs::read_to_string(json_file).map_err(|e| e.to_string())?;
     let commits: Vec<CommitData> = serde_json::from_str(&data).map_err(|e| e.to_string())?;
 
@@ -35,4 +35,33 @@ pub fn amend_commits(json_file: &str, new_author: &str, new_email: &str) -> Resu
     }
 
     Ok(())
+}
+
+
+use std::fs;
+use std::process::Command;
+
+fn main() {
+    // ... your existing code ...
+
+    // Step 1: Parse the commit_data.json
+    let commit_data = parse_commit_data("commit_data.json").expect("Error parsing commit data");
+
+    // Step 2: Write the sequence.editor script
+    let script_content = create_sequence_editor_script(&commit_data);
+    fs::write("sequence_editor.sh", &script_content).expect("Error writing script");
+
+    // Step 3: Configure Git to use the script
+    Command::new("git")
+        .args(["config", "sequence.editor", "sh sequence_editor.sh"])
+        .status()
+        .expect("Failed to set sequence.editor");
+
+    // Step 4: Start the interactive rebase
+    Command::new("git")
+        .args(["rebase", "-i", "--root"]) // adjust as needed
+        .status()
+        .expect("Rebase failed");
+
+    // Cleanup: Remove or reset the sequence.editor configuration if necessary
 }
